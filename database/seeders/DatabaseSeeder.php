f4a6568c8b8de8d29d244b2853b2051bc6dfc9a8
Teddy Pascual <teddylayson2000@gmail.com>
2026-07-19 02:00:50 +0800

    Seed database with initial user data for admin and farmers

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,8 +19,32 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Teddy Pascual',
+            'role' => 'admin',
+            'email' => 'ted@agri.com',
+            'phone' => '09982409945',
+            'password' => Hash::make('pass1234'),
+        ]);
+        User::factory()->create([
+            'name' => 'Denmark Cube',
+            'role' => 'farmer',
+            'email' => 'den@agri.com',
+            'phone' => '09123459223',
+            'password' => Hash::make('pass1234'),
+        ]);
+        User::factory()->create([
+            'name' => 'Darren Ventura',
+            'role' => 'farmer',
+            'email' => 'dar@agri.com',
+            'phone' => '09434433212',
+            'password' => Hash::make('pass1234'),
+        ]);
+        User::factory()->create([
+            'name' => 'Angelica Pascual',
+            'role' => 'consumer',
+            'email' => 'lyka@agri.com',
+            'phone' => '09501809299',
+            'password' => Hash::make('pass1234'),
         ]);
     }
 }
